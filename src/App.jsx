b590ae200da5881d8645edf488c3d7e96d9154dd
wahidul <wahidulhassan60@gmail.com>
2025-08-04 13:35:30 +0600

    Coral repo Create

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { createBrowserRouter,createRoutesFromElements,Route,RouterProvider } from "react-router-dom"
+
+const router = createBrowserRouter (
+  createRoutesFromElements (
+    <Route>
+      
+    </Route>
+  )
+)
+
+
+function App() {
+ 
+
+  return (
+    <>
+      <RouterProvider/>
+    </>
+  )
+}
+
+export default App
